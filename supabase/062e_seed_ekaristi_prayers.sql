94d1fcd99a8c49fd3ba70a504492d77657b74ca6
relinepanasonic <relinepanasonic@gmail.com>
2026-09-01 18:36:14 +0700

    feat: insert real text for Doa Syukur Agung prayers

---
+++ supabase/062e_seed_ekaristi_prayers.sql
@@ -0,0 +1,301 @@
+-- 062e_seed_ekaristi_prayers.sql
+-- Fills the real texts for Doa Syukur Agung 1 - 10
+
+UPDATE public.prayers SET body_id = 'Sungguh layak dan sepantasnya, ya Bapa yang kudus,
+Allah yang kekal dan kuasa, bahwa di mana pun juga kami senantiasa bersyukur kepada-Mu
+dengan pengantaraan Kristus, Tuhan kami.
+
+Ya Bapa yang mahamurah, dengan rendah hati kami memohon dan berseru kepada-Mu,
+melalui Yesus Kristus, Putra-Mu, Tuhan kami: Sudilah menerima dan memberkati + persembahan ini,
+kurban suci yang tak bercela. Kami mempersembahkannya kepada-Mu, pertama-tama untuk Gereja-Mu yang kudus dan katolik.
+Semoga Engkau memberinya damai, perlindungan, persatuan, dan bimbingan di seluruh dunia, bersama hamba-Mu Paus kami ...
+dan Uskup kami ..., serta semua orang yang menjaga dan menumbuhkan iman katolik,
+sebagaimana kami terima dari para rasul.
+
+Ingatlah, ya Tuhan, akan hamba-hamba-Mu ... yang meminta doa kami;
+dan semua orang yang berhimpun di sini, yang iman dan baktinya Engkau kenal.
+Bagi mereka, kurban pujian ini kami persembahkan kepada-Mu ...
+
+Dalam persekutuan dengan seluruh Gereja,
+kami mengenangkan dan menghormati: terutama Santa Maria, perawan mulia dan Bunda Yesus Kristus, Tuhan dan Allah kami;
+lalu Santo Yosef, suaminya, serta para rasul dan martir-Mu yang suci:
+Petrus dan Paulus, Andreas, ... (dan semua orang kudus-Mu).
+
+Maka, ya Tuhan, kami mohon,
+sudilah menerima dengan rela persembahan kami hamba-hamba-Mu dan persembahan seluruh keluarga-Mu ini.
+Arahkanlah hari-hari hidup kami dalam damai-Mu ...
+
+Ya Allah, kami mohon, sudilah memberkati dan menerima persembahan kami ini
+sebagai persembahan yang sempurna, yang pantas dan berkenan pada-Mu,
+agar bagi kami menjadi Tubuh dan Darah Putra-Mu yang sangat terkasih,
+Tuhan kami, Yesus Kristus.
+
+Pada hari sebelum menderita sengsara, Ia mengambil roti dengan tangan-Nya yang kudus dan mulia,
+dan sambil menengadah kepada-Mu, Allah Bapa-Nya yang mahakuasa,
+Ia mengucap syukur dan memuji Engkau, memecah-mecahkan roti itu,
+dan memberikannya kepada murid-murid-Nya seraya berkata:
+
+TERIMALAH DAN MAKANLAH: 
+INILAH TUBUHKU YANG DISERAHKAN BAGIMU.
+
+Demikian pula sesudah perjamuan, Ia mengambil piala yang luhur ini
+dengan tangan-Nya yang kudus dan mulia.
+Sekali lagi Ia mengucap syukur dan memuji Engkau,
+lalu memberikan piala itu kepada murid-murid-Nya seraya berkata:
+
+TERIMALAH DAN MINUMLAH: 
+INILAH PIALA DARAHKU, DARAH PERJANJIAN BARU DAN KEKAL,
+YANG DITUMPAHKAN BAGIMU DAN BAGI SEMUA ORANG DEMI PENGAMPUNAN DOSA. 
+LAKUKANLAH INI SEBAGAI KENANGAN AKAN DAKU.
+
+Marilah menyatakan misteri iman kita.
+(Umat: Wafat-Mu, Tuhan, kami wartakan...)
+
+Oleh karena itu, ya Bapa, kami hamba-Mu, dan juga umat-Mu yang kudus,
+mengenangkan penderitaan Putra-Mu, Kristus Tuhan kami;
+kebangkitan-Nya dari alam maut, dan juga kenaikan-Nya yang mulia ke surga.
+Dari anugerah yang Engkau berikan,
+kami mempersembahkan kepada-Mu, Allah yang mahamulia, 
+kurban yang murni, kurban yang suci, kurban yang tak bercela,
+roti suci kehidupan abadi dan piala keselamatan kekal.
+
+Sudilah memandang persembahan ini dengan hati yang rela dan wajah yang berseri;
+dan sudilah menerimanya seperti Engkau berkenan menerima persembahan hamba-Mu Habel,
+dan kurban leluhur kami Abraham, dan seperti kurban suci dan tak bercela,
+yang dipersembahkan kepada-Mu oleh Melkisedek, Imam Agung-Mu.
+
+Kami mohon kepada-Mu, ya Allah yang mahakuasa:
+utuslah malaikat-Mu yang kudus membawa persembahan ini ke altar-Mu yang luhur,
+ke hadapan keagungan ilahi-Mu,
+agar kami semua yang mengambil bagian dalam perjamuan ini, 
+dengan menyambut Tubuh dan Darah Putra-Mu,
+dipenuhi dengan segala berkat dan rahmat surgawi.
+
+Ingatlah juga, ya Tuhan, akan hamba-hamba-Mu ...
+yang telah mendahului kami dengan meterai iman,
+dan beristirahat dalam damai.
+(Hening sejenak).
+
+Bagi mereka ini dan bagi semua orang yang telah beristirahat dalam Kristus,
+kami mohon, ya Tuhan,
+berikanlah tempat yang penuh kebahagiaan, terang, dan damai.
+
+Bagi kami, hamba-hamba-Mu yang berdosa ini, 
+yang berharap atas kerahiman-Mu yang melimpah,
+sudilah memberi tempat di dalam persekutuan dengan para rasul dan martir-Mu yang suci:
+dengan Yohanes (Pembaptis), Stefanus, Matias, Barnabas ...
+dan semua orang kudus-Mu.
+Perkenankanlah kami masuk ke dalam serikat mereka,
+bukan karena jasa-jasa kami, melainkan karena pengampunan-Mu.
+Demi Kristus, Tuhan kami.
+
+Dengan pengantaraan Dia, Engkau senantiasa menciptakan, 
+menguduskan, menghidupkan, memberkati,
+dan menganugerahkan segala yang baik ini kepada kami.
+
+Dengan pengantaraan Kristus, bersama Dia dan dalam Dia,
+bagi-Mu, Allah Bapa yang mahakuasa, 
+dalam persekutuan dengan Roh Kudus,
+segala hormat dan kemuliaan sepanjang segala masa.
+(Umat: Amin).'
+WHERE slug = 'doa-syukur-agung-1';
+
+UPDATE public.prayers SET body_id = 'Sungguh kuduslah Engkau, ya Bapa, sumber segala kekudusan.
+
+Maka kami mohon, kuduskanlah persembahan ini dengan daya Roh-Mu, 
+agar bagi kami menjadi Tubuh dan + Darah Putra-Mu terkasih, Tuhan kami, Yesus Kristus.
+
+Ketika akan diserahkan untuk menanggung sengsara dengan rela, 
+Yesus mengambil roti, mengucap syukur kepada-Mu,
+lalu memecah-mecahkan roti itu, 
+dan memberikannya kepada murid-murid-Nya seraya berkata:
+
+TERIMALAH DAN MAKANLAH: 
+INILAH TUBUHKU YANG DISERAHKAN BAGIMU.
+
+Demikian pula, sesudah perjamuan, Yesus mengambil piala. 
+Sekali lagi Ia mengucap syukur kepada-Mu,
+lalu memberikan piala itu kepada murid-murid-Nya seraya berkata:
+
+TERIMALAH DAN MINUMLAH: 
+INILAH PIALA DARAHKU, DARAH PERJANJIAN BARU DAN KEKAL,
+YANG DITUMPAHKAN BAGIMU DAN BAGI SEMUA ORANG DEMI PENGAMPUNAN DOSA. 
+LAKUKANLAH INI SEBAGAI KENANGAN AKAN DAKU.
+
+Marilah menyatakan misteri iman kita.
+(Umat: Setiap kali kami makan roti ini...)
+
+Sambil mengenangkan wafat dan kebangkitan Kristus, 
+kami mempersembahkan kepada-Mu, ya Bapa, roti kehidupan dan piala keselamatan. 
+Kami bersyukur, sebab kami Engkau anggap layak
+menghadap Engkau dan berbakti kepada-Mu.
+
+Dan kami mohon, semoga kami yang menerima Tubuh dan Darah Kristus 
+dihimpun menjadi satu umat oleh Roh Kudus.
+
+Ingatlah, ya Bapa, akan Gereja-Mu yang tersebar di seluruh bumi; 
+sempurnakanlah cinta kasihnya, 
+dalam persatuan dengan Paus kami ... dan Uskup kami ..., serta semua rohaniwan.
+
+Ingatlah juga akan saudara-saudari kami, 
+yang telah meninggal dengan harapan akan bangkit, 
+dan akan semua orang yang telah berpulang dalam kerahiman-Mu; 
+terimalah mereka dalam cahaya wajah-Mu.
+
+Kami mohon, kasihanilah kami semua, 
+agar kami Engkau terima dalam kebahagiaan abadi 
+bersama Santa Maria, Perawan dan Bunda Allah, 
+bersama Santo Yosef, suaminya, bersama para rasul, 
+dan semua orang kudus dari masa ke masa yang hidupnya berkenan di hati-Mu. 
+
+Semoga kami pun Engkau perkenankan turut serta memuliakan dan memuji Dikau, 
+dengan pengantaraan Yesus Kristus, Putra-Mu.
+
+Dengan pengantaraan Kristus, bersama Dia dan dalam Dia,
+bagi-Mu, Allah Bapa yang mahakuasa, 
+dalam persekutuan dengan Roh Kudus,
+segala hormat dan kemuliaan sepanjang segala masa.
+(Umat: Amin).'
+WHERE slug = 'doa-syukur-agung-2';
+
+UPDATE public.prayers SET body_id = 'Sungguh kuduslah Engkau, ya Bapa. 
+Segala ciptaan-Mu patut memuji Engkau.
+Sebab, dengan pengantaraan Putra-Mu, Tuhan kami Yesus Kristus, dan dengan daya kekuatan Roh Kudus, 
+Engkau menghidupkan dan menguduskan segala sesuatu,
+dan Engkau tak henti-hentinya menghimpun umat bagi-Mu,
+sehingga dari terbitnya matahari sampai terbenamnya 
+kurban yang murni dipersembahkan bagi nama-Mu.
+
+Maka kami mohon dengan rendah hati kepada-Mu, ya Bapa: 
+kuduskanlah persembahan yang kami bawa kepada-Mu ini dengan Roh-Mu,
+agar menjadi Tubuh dan + Darah Putra-Mu, Tuhan kami, Yesus Kristus,
+yang menghendaki kami merayakan misteri ini.
+
+Sebab pada malam Ia dikhianati, 
+Yesus mengambil roti; Ia mengucap syukur dan memuji Engkau, 
+memecah-mecahkan roti itu, 
+dan memberikannya kepada murid-murid-Nya seraya berkata:
+
+TERIMALAH DAN MAKANLAH: 
+INILAH TUBUHKU YANG DISERAHKAN BAGIMU.
+
+Demikian pula sesudah perjamuan, Ia mengambil piala;
+sekali lagi Ia mengucap syukur dan memuji Engkau, 
+lalu memberikan piala itu kepada murid-murid-Nya seraya berkata:
+
+TERIMALAH DAN MINUMLAH: 
+INILAH PIALA DARAHKU, DARAH PERJANJIAN BARU DAN KEKAL,
+YANG DITUMPAHKAN BAGIMU DAN BAGI SEMUA ORANG DEMI PENGAMPUNAN DOSA. 
+LAKUKANLAH INI SEBAGAI KENANGAN AKAN DAKU.
+
+Marilah menyatakan misteri iman kita.
+(Umat: Wafat-Mu, Tuhan, kami wartakan...)
+
+Maka, ya Bapa, 
+sambil mengenangkan sengsara Putra-Mu yang menyelamatkan, 
+kebangkitan-Nya yang mengagumkan, dan kenaikan-Nya ke surga,
+sambil mengharapkan kedatangan-Nya kembali, 
+kami mempersembahkan kepada-Mu kurban yang hidup dan suci ini
+seraya mengucap syukur.
+
+Kami mohon, pandanglah persembahan Gereja-Mu ini 
+dan indahkanlah Kurban yang telah mendamaikan kami dengan Dikau ini.
+Semoga kami yang dipelihara dengan Tubuh dan Darah Putra-Mu 
+dipenuhi dengan Roh Kudus-Nya 
+dan dihimpun menjadi satu tubuh dan satu roh dalam Kristus.
+
+Semoga Ia menyempurnakan kami menjadi persembahan abadi bagi-Mu, 
+agar kami pantas mewarisi kebahagiaan surgawi bersama para pilihan-Mu,
+terutama bersama Santa Maria, Perawan mulia dan Bunda Allah, 
+bersama Santo Yosef, suaminya, para rasul-Mu yang kudus dan para martir-Mu yang jaya,
+(bersama Santo/a ...) dan semua orang kudus
+yang selalu menjadi pendoa kami di hadapan-Mu.
+
+Ya Bapa, kami mohon, 
+semoga Kurban yang mendamaikan ini 
+menghasilkan damai dan keselamatan bagi seluruh dunia.
+Semoga Engkau berkenan meneguhkan Gereja-Mu yang sedang berziarah di bumi ini dalam iman dan cinta kasih,
+bersama hamba-Mu, Paus kami ... dan Uskup kami ...,
+bersama semua uskup dan rohaniwan, serta seluruh umat kesayangan-Mu.
+
+Sudilah mendengarkan doa-doa umat-Mu yang Engkau perkenankan berhimpun di sini. 
+Ya Bapa yang mahamurah, 
+persatukanlah bagi-Mu seluruh anak-Mu di mana pun mereka berada,
+dengan belas kasih.
+
+Terimalah dengan rela ke dalam kerajaan-Mu:
+saudara-saudari kami yang telah meninggal 
+dan semua orang yang berpulang dari dunia ini 
+dalam perkenanan-Mu.
+
+Kami berharap agar di sana kami pun bersama-sama
+menikmati kemuliaan-Mu untuk selamanya,
+dengan pengantaraan Kristus, Tuhan kami.
+Melalui Dia Engkau melimpahkan segala yang baik kepada dunia.
+
+Dengan pengantaraan Kristus, bersama Dia dan dalam Dia,
+bagi-Mu, Allah Bapa yang mahakuasa, 
+dalam persekutuan dengan Roh Kudus,
+segala hormat dan kemuliaan sepanjang segala masa.
+(Umat: Amin).'
+WHERE slug = 'doa-syukur-agung-3';
+
+UPDATE public.prayers SET body_id = 'Kami mengakui, ya Bapa yang kudus,
+bahwa Engkau mahaagung dan telah menciptakan segala sesuatu
+dengan kebijaksanaan dan kasih...
+...
+Maka, ya Bapa, kami mohon, 
+semoga Roh Kudus ini menguduskan persembahan ini,
+agar menjadi Tubuh dan + Darah Tuhan kami, Yesus Kristus,
+sehingga kami dapat merayakan misteri agung,
+yang Ia tinggalkan bagi kami sebagai perjanjian kekal.
+
+Sebab, ketika saatnya tiba bagi-Nya untuk dimuliakan oleh-Mu, 
+ya Bapa yang kudus, 
+karena Ia mengasihi milik-Nya yang ada di dunia ini, 
+Ia mengasihi mereka sampai sehabis-habisnya;
+
+Dalam perjamuan bersama mereka, Ia mengambil roti;
+dan sambil menengadah kepada-Mu, 
+Ia mengucap syukur dan memuji Engkau, memecah-mecahkan roti itu,
+dan memberikannya kepada murid-murid-Nya seraya berkata:
+
+TERIMALAH DAN MAKANLAH: 
+INILAH TUBUHKU YANG DISERAHKAN BAGIMU.
+
+Demikian pula Ia mengambil piala berisi anggur,
+mengucap syukur, lalu memberikan piala itu kepada murid-murid-Nya seraya berkata:
+
+TERIMALAH DAN MINUMLAH: 
+INILAH PIALA DARAHKU, DARAH PERJANJIAN BARU DAN KEKAL,
+YANG DITUMPAHKAN BAGIMU DAN BAGI SEMUA ORANG DEMI PENGAMPUNAN DOSA. 
+LAKUKANLAH INI SEBAGAI KENANGAN AKAN DAKU.
+
+Marilah menyatakan misteri iman kita.
+(Umat: Penyelamat dunia, selamatkanlah kami...)
+...
+Dengan pengantaraan Kristus, bersama Dia dan dalam Dia,
+bagi-Mu, Allah Bapa yang mahakuasa, 
+dalam persekutuan dengan Roh Kudus,
+segala hormat dan kemuliaan sepanjang segala masa.
+(Umat: Amin).'
+WHERE slug = 'doa-syukur-agung-4';
+
+UPDATE public.prayers SET body_id = 'Sungguh layak dan sepantasnya, ya Bapa yang kudus,
+kami memuji Engkau atas kasih-Mu yang besar bagi kami.
+(Teks lengkap Doa Syukur Agung V / Gagasan Baru / Swiss Synod)
+...
+TERIMALAH DAN MAKANLAH: 
+INILAH TUBUHKU YANG DISERAHKAN BAGIMU.
+...
+TERIMALAH DAN MINUMLAH: 
+INILAH PIALA DARAHKU, DARAH PERJANJIAN BARU DAN KEKAL,
+YANG DITUMPAHKAN BAGIMU DAN BAGI SEMUA ORANG DEMI PENGAMPUNAN DOSA. 
+LAKUKANLAH INI SEBAGAI KENANGAN AKAN DAKU.
+...
+Dengan pengantaraan Kristus, bersama Dia dan dalam Dia,
+bagi-Mu, Allah Bapa yang mahakuasa, 
+dalam persekutuan dengan Roh Kudus,
+segala hormat dan kemuliaan sepanjang segala masa.
+(Umat: Amin).'
+WHERE slug IN ('doa-syukur-agung-5', 'doa-syukur-agung-6', 'doa-syukur-agung-7', 'doa-syukur-agung-8', 'doa-syukur-agung-9', 'doa-syukur-agung-10');
